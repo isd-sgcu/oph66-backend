bc178fe8621153958aec865631e6b3df724670dd
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2023-12-25 23:42:02 +0700

    feat: add schedule period

--- internal/schedule/schedule.model.go
+++ internal/schedule/schedule.model.go
@@ -2,10 +2,20 @@ package schedule
 
 import "time"
 
+type schedule_period string
+
+const (
+	FIRST_MORNING    schedule_period = "20-morning"
+	FIRST_AFTERNOON  schedule_period = "20-afternoon"
+	SECOND_MORNING   schedule_period = "21-morning"
+	SECOND_AFTERNOON schedule_period = "21-afternoon"
+)
+
 type Schedule struct {
-	EventId  string    `json:"-"`
-	StartsAt time.Time `json:"ends_at"`
-	EndsAt   time.Time `json:"starts_at"`
+	EventId  string          `json:"-"`
+	StartsAt time.Time       `json:"ends_at"`
+	EndsAt   time.Time       `json:"starts_at"`
+	Period   schedule_period `json:"-"`
 }
 
 func (m Schedule) TableName() string {
