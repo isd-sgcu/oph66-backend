52c9a4118812cac22c99825b8e43a5de5d442be9
AK Katung <6434496423@student.chula.ac.th>
2023-12-26 20:29:44 +0700

    fix:typo, config linters

--- internal/auth/auth.handler.go
+++ internal/auth/auth.handler.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/isd-sgcu/oph66-backend/apperror"	
+	"github.com/isd-sgcu/oph66-backend/apperror"
 	"github.com/isd-sgcu/oph66-backend/utils"
 	"go.uber.org/zap"
 )
