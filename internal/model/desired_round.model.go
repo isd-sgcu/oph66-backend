c08b2d519fc5d3f1293241bcbadcd78ae6767d49
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2024-01-02 01:49:47 +0700

    fix: lint

--- internal/model/desired_round.model.go
+++ internal/model/desired_round.model.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type DesiredRound struct {
-	UserId    uint      `gorm:"primaryKey;index" json:"-"`
+	UserId    uint      `gorm:"primaryKey;index"     json:"-"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	UpdatedAt time.Time `gorm:"autoUpdateTime:milli"`
-	Order     uint      `gorm:"primaryKey"       json:"order"`
-	Round     Round     `gorm:""                 json:"round"`
+	Order     uint      `gorm:"primaryKey"           json:"order"`
+	Round     Round     `gorm:""                     json:"round"`
 }
 
 func (d DesiredRound) TableName() string {
